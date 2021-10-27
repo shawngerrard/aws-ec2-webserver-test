35d9db90af6c1a6f61db88ff59acd88723b6dce9
shawn@Mandalore <shawn.gerrard@gmail.com>
2021-10-27 13:28:36 +1300

    Included tags in EC2 instance definition and installed Kubectl into Pulumi project

--- pulumi/__main__.py
+++ pulumi/__main__.py
@@ -17,14 +17,27 @@ group = aws.ec2.SecurityGroup('administrator-sg-litrepublicpoc',
     description='Enable SSH and HTTP access for Lit Republic',
     ingress=[
         { 'protocol': 'tcp', 'from_port': 22, 'to_port': 22, 'cidr_blocks': [extip.text.strip()+'/32'] },
-        { 'protocol': 'tcp', 'from_port': 8080, 'to_port': 8080, 'cidr_blocks': ['0.0.0.0/0'] }
+        { 'protocol': 'tcp', 'from_port': 443, 'to_port': 443, 'cidr_blocks': ['0.0.0.0/0'] }
     ])
+
+# Define the WWW start-up scripting
+user_data = """
+#!/bin/bash
+curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
+sudo install minikube-linux-amd64 /usr/local/bin/minikube
+echo "<html><head><title>Lit Republic WWW Test</title></head><body>Well, helo thar fren!</body></html>" > index.html
+nohup python -m SimpleHTTPServer 443 &
+"""
 
 # Define the AWS EC2 instance to start
 server = aws.ec2.Instance('litrepublicpoc-www',
     instance_type=size,
-    vpc_security_group_ids=[group.id], # reference security group from above
-    ami=ami.id)
+    vpc_security_group_ids=[group.id], # Reference security group from above
+    user_data = user_data, # Reference user data above
+    ami=ami.id,
+    tags={
+        "Name":"litrepublicpoc-ec2"
+    })
 
 # Export the public IP and hostname of the Amazon server
 pulumi.export('publicIp', server.public_ip)
